4c6de2cb84b0c3e22f083758fb071afb551fcbee
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-18 10:40:21 -0500

    added db.js and sourced in html

--- server.js
+++ server.js
@@ -19,12 +19,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-
+//modified from original pathway and object params
+mongoose.connect(
+  process.env.MONGODB_URI || `mongodb://localhost:27017/${process.env.DB_NAME}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
